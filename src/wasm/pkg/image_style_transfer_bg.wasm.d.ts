114328a586b12212fcdf6111585233fa702594b0
Yonathan Daniel <87887705+yond5413@users.noreply.github.com>
2025-08-25 02:04:19 +0000

    checkpoint before webcam version and styling ui

---
+++ src/wasm/pkg/image_style_transfer_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const preprocess: (a: number, b: number, c: number, d: number) => [number, number, number];
+export const postprocess: (a: any, b: number, c: number, d: number, e: number, f: number) => [number, number, number];
+export const add: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
